9f6b486f8b2bc7c4776fed43f406e37262b1194b
shmsulhaque <shmsulhaque@192.168.0.110>
2018-05-20 20:40:59 +0530

    initial commit and push

---
+++ datastructures-practice/src/com/shams/list/singly/LinkedListApp.java
@@ -0,0 +1,89 @@
+package com.shams.list.singly;
+
+import java.util.Scanner;
+
+public class LinkedListApp {
+	static Scanner sc = new Scanner(System.in);
+	Node head = new Node(-1);
+	public static void main(String[] args) {
+		LinkedListApp app = new LinkedListApp();
+		app.insertSorted(3);
+		app.insertSorted(2);
+		app.insertSorted(1);
+		app.insertSorted(1);
+		app.insertSorted(1);
+		app.insertSorted(100);
+		app.insertSorted(1);
+		app.insertSorted(10);
+		app.insertSorted(1);
+		app.printList();
+		/*int inputs = 0;
+		while((inputs = promptInput()) > 0) {
+			LinkedListApp app = new LinkedListApp();
+			int c = 1;
+			while( inputs > 0) {
+				System.out.print("Enter value :>> "+ (inputs - (inputs - c)));
+				app.insertSorted(Integer.valueOf(sc.nextLine()));
+				inputs --;
+				c++;
+			}
+			app.printList();
+			break;
+		}*/
+
+	}
+	private void insertSorted(int val) {
+		Node newNode = new Node(val);
+		if(head.next == null) {
+			head.next = newNode;
+		} else {
+			Node last = head.next;
+			while(last != null) {
+				if(last.value >= val) {
+				Node temp = 	last;
+				last = newNode; 
+				newNode.next = temp;
+				head.next = last;
+				break;
+				} else if(last.value < val && last.next == null) {
+					last.next = newNode;
+					break;
+				} else {
+					last = last.next;
+				}
+			}
+		}
+	}
+	private static int promptInput () {
+		System.out.print("Vaule to be entered :");
+		int numOfInputs = Integer.parseInt(sc.nextLine());
+		return numOfInputs;
+	}
+	//append
+	private Node insert(int val) {
+		Node newNode = new Node(val);
+		if(head.next == null) {
+			head.next = newNode;
+		} else {
+			Node last = head.next;
+			while(last.next != null) {
+				last = last.next;
+			}
+			last.next = newNode;
+		}
+		return head;
+	}
+
+	
+	private void printList() {
+		if(head.next == null) {
+			System.out.println(head.value);
+		} else {
+			System.out.print("Values : ");
+			while(head.next != null) {
+				System.out.print(head.next.value +" ");
+				head.next = head.next.next;
+			}
+		}
+	}
+}
---
+++ datastructures-practice/src/com/shams/list/singly/Node.java
@@ -0,0 +1,21 @@
+package com.shams.list.singly;
+
+public class Node {
+	
+	public int value;
+	public Node next;
+	/**
+	 * @param value
+	 * @param next
+	 */
+	public Node(int value) {
+		this.value = value;
+		this.next = null;
+	}
+	/**
+	 *  Default Contructor
+	 */
+	public Node() {
+	}
+	
+}
